0192ff6d5775c20657a679b327ec3103adf122a2
Justin Healey <jjheals@gmail.com>
2024-09-14 15:17:58 -0400

    add utils.js

---
+++ app/web-ui/src/utils.js
@@ -0,0 +1,7 @@
+export function toTitleCase(str) {
+    return str
+        .toLowerCase() // Convert the string to lowercase
+        .split(' ') // Split the string into words
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+        .join(' '); // Join the words back into a single string
+}
